4e2329b5caf3c856d1efdde86c0c2a4d7f56e8a1
Hung Nguyen <hung.nguyen04@hastech.vn>
2023-08-23 23:14:31 +0700

    Implemented mode asynch

--- src/main/java/vn/edu/hcmut/nxvhung/bloomserver/BloomServerApplication.java
+++ src/main/java/vn/edu/hcmut/nxvhung/bloomserver/BloomServerApplication.java
@@ -3,9 +3,11 @@ package vn.edu.hcmut.nxvhung.bloomserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJms
+@EnableScheduling
 public class BloomServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/vn/edu/hcmut/nxvhung/bloomserver/service/BlacklistService.java
+++ src/main/java/vn/edu/hcmut/nxvhung/bloomserver/service/BlacklistService.java
@@ -1,16 +1,18 @@
 package vn.edu.hcmut.nxvhung.bloomserver.service;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import vn.edu.hcmut.nxvhung.bloomfilter.Filterable;
 import vn.edu.hcmut.nxvhung.bloomfilter.dto.Message;
@@ -30,7 +32,11 @@ public class BlacklistService {
   @Value("${mode.async}")
   private boolean async;
 
-  private AtomicInteger currentTimestamp = new AtomicInteger(0);
+  @Value("${mode.epsilon}")
+  private Integer epsilon;
+
+  private static final AtomicInteger currentTimestamp = new AtomicInteger(0);
+  private static final Map<String, Integer> maxTimestampsMap = new ConcurrentHashMap<>();
 
   public BlacklistService(BlacklistSender blacklistSender, CompaniesSetting companiesSetting) {
     this.blacklistSender = blacklistSender;
@@ -41,8 +47,9 @@ public class BlacklistService {
   public void handleBlacklist(Message message) {
     String companyName = message.getCompanyName();
     currentTimestamp.set(Math.max(message.getTimestamp(), currentTimestamp.get()));
+    maxTimestampsMap.put(companyName, message.getTimestamp());
     Map<String, CompanyData> blacklistData = blacklistCompanyByTimestamp.get(message.getTimestamp());
-    if(Objects.isNull(blacklistData)) {
+    if (Objects.isNull(blacklistData)) {
       blacklistData = new HashMap<>();
       blacklistCompanyByTimestamp.put(message.getTimestamp(), blacklistData);
     }
@@ -60,9 +67,9 @@ public class BlacklistService {
 
   private void mergeAndSendBack(String companyName) throws CloneNotSupportedException {
     List<String> partners = companiesSetting.getRelatedCompanies(companyName);
-    boolean canSych  = canSynch(partners);
+    boolean canSyc = canSync(partners);
 
-    if(canSych) {
+    if (canSyc) {
       Map<String, CompanyData> blacklistMap = blacklistCompanyByTimestamp.get(currentTimestamp.get());
       Filterable<Key> mergedBlacklist = (Filterable<Key>) ((Filterable<Key>) blacklistMap.get(companyName).getBlacklist()).clone();//get a copy
       partners.forEach(p -> mergedBlacklist.merge(blacklistMap.get(p).getBlacklist()));
@@ -70,15 +77,42 @@ public class BlacklistService {
     }
   }
 
-  private boolean canSynch(List<String> partners) {
+  private boolean canSync(List<String> partners) {
     Map<String, CompanyData> blacklistMap = blacklistCompanyByTimestamp.get(currentTimestamp.get());
+    return partners.stream().allMatch(blacklistMap::containsKey);
+  }
 
-    if(!async) {
-      return partners.stream().allMatch(blacklistMap::containsKey);
+  @Scheduled(cron = "10 * * * *")
+  public void checkAndSendToTheClientsInAsyncMode() {
+    if (!async) {
+      return;
+    }
+    Map<String, CompanyData> currentBlacklist = blacklistCompanyByTimestamp.get(currentTimestamp.get());
+
+    for (Entry<String, CompanyData> entry : currentBlacklist.entrySet()) {
+      processByCompany(entry);
+    }
+  }
+
+  private void processByCompany(Entry<String, CompanyData> entry)  {
+    String companyName = entry.getKey();
+    List<String> partners = companiesSetting.getRelatedCompanies(companyName);
+    boolean canSync = partners.stream().allMatch(p -> Math.abs(currentTimestamp.get() - maxTimestampsMap.getOrDefault(p, 0)) > epsilon);
+    if(!canSync) {
+      return;
     }
 
+    Map<String, CompanyData> blacklistMap = blacklistCompanyByTimestamp.get(currentTimestamp.get());
+    try {
+      Filterable<Key> mergedBlacklist = (Filterable<Key>) ((Filterable<Key>) blacklistMap.get(companyName).getBlacklist()).clone();
+      partners.forEach(p -> {
+        Optional<Filterable<Key>> blacklist = Optional.ofNullable(blacklistCompanyByTimestamp.get(maxTimestampsMap.getOrDefault(p, 0))).map(CompanyData::getBlacklist);
+        blacklist.ifPresent(mergedBlacklist::merge);
+      });
+    } catch (CloneNotSupportedException e) {
+      throw new RuntimeException(e);
+    }
 
-    return false;
   }
 
 
